dab5fd31e0c1f7d9b7b6ad226c478b9da6579f09
kazesberger <klaus.azesberger@bearingpoint.com>
2018-10-17 00:12:52 +0200

    Solve 4clj105 (identify keys/vals)

--- src/foreclojure/4clj105.clj
+++ src/foreclojure/4clj105.clj
@@ -1,1 +1,19 @@
 (ns foreclojure.4clj105)
+
+(partition-by type [:a 1 2 3 :b :c 4])
+
+
+( (fn kv-map [coll]
+    (let [basemap (into {} (map #(vector % []) (filter keyword? coll)))]
+      (->> coll
+           (partition-by type)
+           (map #(if (keyword? (first %)) (last %) %))
+           (partition 2)
+           (map vec)
+           (into {})
+           (merge basemap))))
+
+ [:a 1 2 3 :b :c 4])
+
+
+(into {} (map #(vector % []) (filter keyword? [:a 1 2 3 :b :c 4])))
